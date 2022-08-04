493110c8fad11fa40f95a67ce4bd23e38d7ea89b
yosra besbes <yosrabesbes17@gmail.com>
2022-08-04 08:59:07 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,34 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png">
+    <h1 class="title">welcome to winopapa</h1>
+    <h2 class="minititle">yosra besbes</h2>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'App',
+  components: {
+   
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+.title{
+  color: rgb(62, 62, 90);
+}
+.minititle{
+  color: rgb(79, 171, 79);
+}
+</style>
